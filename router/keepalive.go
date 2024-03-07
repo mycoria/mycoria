bd85bfbb74b66467174e77fbcaa34334a5484885
Daniel <dhaavi@users.noreply.github.com>
2024-03-07 10:42:07 +0100

    Improve keep alive to react faster

--- router/keepalive.go
+++ router/keepalive.go
@@ -8,26 +8,47 @@ import (
 )
 
 func (r *Router) keepAliveWorker(w *mgr.WorkerCtx) error {
-	ticker := time.NewTicker(15 * time.Second)
+	lastCheck := time.Now()
+	ticker := time.NewTicker(time.Second)
+	var ticks uint64
+
 	for {
 		select {
 		case <-w.Done():
 			return nil
 		case <-ticker.C:
-			r.keepAlivePeers(w)
+			ticks++
+			switch {
+			case clockTimeSince(lastCheck) > 5*time.Minute:
+				// Check with fast fail when last check is more than 5 minutes ago.
+				// This will usually happen when device was sleeping.
+				lastCheck = time.Now()
+				r.keepAlivePeers(w, true)
+
+			case ticks%15 == 0:
+				// Check every 15 ticks / 15 sec.
+				lastCheck = time.Now()
+				r.keepAlivePeers(w, false)
+			}
 		}
 	}
 }
 
-func (r *Router) keepAlivePeers(w *mgr.WorkerCtx) {
+// clockTimeSince returns the elapsed non-monotic time since t.
+// This can have weird results when the system clock changes.
+func clockTimeSince(t time.Time) time.Duration {
+	return time.Duration(time.Now().Unix()-t.Unix()) * time.Second
+}
+
+func (r *Router) keepAlivePeers(w *mgr.WorkerCtx, fastCheck bool) {
 	for _, link := range r.instance.Peering().GetLinks() {
-		// Skip incoming and closing connections.
-		if !link.Outgoing() || link.IsClosing() {
+		// Skip closing connections.
+		if link.IsClosing() {
 			continue
 		}
 
 		// Keep alive peer.
-		r.keepAlivePeer(w, link)
+		r.keepAlivePeer(w, link, fastCheck)
 
 		// Check if worker is canceled.
 		if w.IsDone() {
@@ -36,51 +57,81 @@ func (r *Router) keepAlivePeers(w *mgr.WorkerCtx) {
 	}
 }
 
-func (r *Router) keepAlivePeer(w *mgr.WorkerCtx, link peering.Link) {
+func (r *Router) keepAlivePeer(w *mgr.WorkerCtx, link peering.Link, fastCheck bool) {
 	var (
-		fails  int
-		notify <-chan struct{}
-		pingID uint64
-		err    error
+		fails       int
+		notify      <-chan struct{}
+		pingID      uint64
+		err         error
+		sentFirstAt = time.Now()
 	)
 
 	for {
+		// Abort if the link is closing.
+		if link.IsClosing() {
+			return
+		}
+
 		// Close if ping fails persistently.
-		if fails >= 5 { // 15 seconds.
+		if fails >= 5 || (fastCheck && fails >= 1) {
 			link.Close(func() {
 				w.Warn(
 					"link seems down, closing",
 					"router", link.Peer(),
+					"fast-check", fastCheck,
 				)
 			})
 			return
 		}
 
 		// Send keep-alive.
-		sentAt := time.Now()
-		notify, pingID, err = r.PingPong.Send(link.Peer(), pingID)
+		notify, pingID, err = r.PingPong.Send(link.Peer(), true, pingID)
 		if err != nil {
+			// Abort silently if the link is closing.
+			if link.IsClosing() {
+				return
+			}
+			fails++
+
 			w.Warn(
 				"failed to send keep-alive ping",
 				"router", link.Peer(),
 				"err", err,
 			)
 
-			fails++
-			time.Sleep(3 * time.Second)
-			continue
+			// Try again after some time.
+			if fastCheck {
+				continue
+			}
+			select {
+			case <-w.Done():
+				return
+			case <-time.After(time.Second):
+				continue
+			}
 		}
 
 		// Wait for response.
 		select {
 		case <-w.Done():
 			return
+
 		case <-notify:
-			// TODO: This might give wrong measurements sometimes, because we retry
-			// pings with the same ID. Impact is quite low though, as we use 10x avg.
-			link.AddMeasuredLatency(time.Since(sentAt) / 2)
+			// We re-use ping IDs, so we always measure from the first ping we sent.
+			// High reported latency is not too bad when we have obvious packet loss.
+			// Impact is also quite low though, as we use 10x avg.
+			link.AddMeasuredLatency(time.Since(sentFirstAt) / 2)
+
+			// Update status if we printed a warning.
+			if fails >= 1 {
+				w.Info(
+					"keep-alive succeeded",
+					"router", link.Peer(),
+				)
+			}
 			return
-		case <-time.After(3 * time.Second):
+
+		case <-time.After(time.Second):
 			fails++
 			w.Warn(
 				"keep-alive timed out",
